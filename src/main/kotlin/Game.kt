307d7fb54e7a67229e0eeedcced0a1a11a25cd46
Taron8530 <102309691+Taron8530@users.noreply.github.com>
2023-03-02 10:44:45 +0900

    코틀린 두번제 과제

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,45 +1,45 @@
+import sun.security.ec.point.ProjectivePoint.Mutable
+
 class Game {
-    val array = arrayOf(arrayOf("elephant","dog","hippo","lion","cat","fox"), arrayOf("apple","banana","peach","watermelon","orange"),
-        arrayOf("korea","japan","china","unitedstates","brazil","russia","unitedkingdom")
+    val array = arrayOf(arrayOf("cow","fox","pig","dog","bat"), arrayOf("mouse","monkey","tiger","hyena","raccoon","panda","eagle"),
+        arrayOf("rhinoceros","crocodile","elephant","jaguar","dolphin","sheep","penguin")
     )
+    fun <T> shuffle(list: MutableList<T>) {
+        list.shuffle()
+    }
     fun Game_start(){
-        println("주제를 선택해주세요")
-        println("1: 동물, 2: 과일, 3: 나라이름")
+        var count = 0
+        println("주제는 동물입니다")
+        println("1:1단계, 2:2단계, 3:3단계")
         var input = readLine()?.toInt()?.minus(1)
+        if(input == 0){
+            count = 10
+        }else if(input == 1){
+            count = 7
+        }else{
+            count = 5
+        }
         var range = array.get(input!!).size
         var answer = array[input!!].get((0..range-1).random())
 //        println(answer)
-        var blenk = ""
-        for (i in (1..answer?.length!! step(1))){
-            blenk += "_"
-        }
-        var blenks = StringBuilder(blenk)
-        var count = blenks.length +3
+        var list : MutableList<Char> = answer.toMutableList()
+
+        shuffle(list)
         while(true){
-            if(count <=0){
+            if(count <= 0){
                 println("GAME OVER")
                 break
             }
+            println(list)
             println("남은 횟수 "+count)
-            println(blenks.toString())
-            if(blenks.indexOf("_")==-1){
+            println("동물 이름을 입력해주세요!")
+            var result = readLine()
+            if(answer.equals(result)){
                 println(answer)
                 println("축하합니다 단어를 맞추셧습니다.")
                 break
             }
-            println("알파벳 한글자만 입력해주세요!")
-            var result = readLine()?.single()
             count--
-            for(i in (0..answer?.length?.minus(1)!! step(1))){
-                if(answer[i].equals(result)) {
-                    blenks.also { it.setCharAt(i, result!!) }
-                }
-            }
-            if(blenks.indexOf("_")==-1){
-                println("축하합니다 ")
-                break
-            }
-
         }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     var game = Game()
-    println("----행 맨 게 임----")
+    println("----동물 이름 맞추기 게임 ----")
     println("게임을 시작하시려면 아무키나 입력해주세요")
     readLine()
     game.Game_start()
